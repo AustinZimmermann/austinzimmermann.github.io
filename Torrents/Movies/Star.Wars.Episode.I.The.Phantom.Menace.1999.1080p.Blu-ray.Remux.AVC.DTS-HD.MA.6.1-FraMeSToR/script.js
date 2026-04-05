4f9c3d9e4166a972c0d2bf2b8b75685d49f48eab
Austin Zimmermann <austinzironic@icloud.com>
2026-04-04 21:37:22 -0400

    Implement download button functionality in script.js
    Add event listener to download button for torrent file.

---
+++ Torrents/Movies/Star.Wars.Episode.I.The.Phantom.Menace.1999.1080p.Blu-ray.Remux.AVC.DTS-HD.MA.6.1-FraMeSToR/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dlBtn = document.getElementById('dlBtn');
+
+    dlBtn.addEventListener('click', () => {
+        alert('Torrent file download started: Star.Wars.Episode.I.The.Phantom.Menace.1999.1080p.Blu-ray.Remux.AVC.DTS-HD.MA.6.1-FraMeSToR.torrent');
+    });
+});
